6b3c60252f8177653210fae6e70d4339ab10f185
Pavlo Makukha <7734.why.not@gmail.com>
2020-02-13 22:07:49 +0200

    Table columns

--- src/pages/Table/Table.jsx
+++ src/pages/Table/Table.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom'
 
 function Table({table}) {
+  const normalizeTable = () => {
+    const normalizedTable  = [];
+
+    const coefficient = table.length / 4;
+
+    for (let i = 0; i < coefficient; i++) {
+      normalizedTable.push([]);
+      normalizedTable[normalizedTable.length - 1]
+        .push({index: i, value: table[i]});
+      normalizedTable[normalizedTable.length - 1]
+        .push({index: i + coefficient, value: table[i + coefficient]});
+      normalizedTable[normalizedTable.length - 1]
+        .push({index: i + coefficient * 2, value: table[i + coefficient * 2]});
+      normalizedTable[normalizedTable.length - 1]
+        .push({index: i + coefficient * 3, value: table[i + coefficient * 3]});
+    }
+
+    return normalizedTable;
+  };
+
   return (
     <> 
       <NavLink className="btn btn-danger" to="/" style={{margin: '20px 0'}}>
@@ -10,10 +30,14 @@ function Table({table}) {
 
       <table className="table table-hover">
         <tbody>
-          {table.map( (element, index) => (
+          {normalizeTable().map( (row, index) => (
             <tr key={index}>
-              <td>{index}.</td>
-              <td dangerouslySetInnerHTML={{__html: element}}></td>
+              {row.map( ({index, value}) => (
+                <Fragment key={index}>
+                  <td>{index}.</td>
+                  <td dangerouslySetInnerHTML={{__html: value}}></td>
+                </Fragment>
+              ))}
             </tr>
           ))};
         </tbody>
